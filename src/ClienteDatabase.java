d8acbca8df03be8585683b88edf327fa5a7a0763
anton <agmsob@gmail.com>
2024-03-19 12:56:49 -0200

    ClienteDatabase agora utiliza serialização para tornar a DB persistente

--- src/ClienteDatabase.java
+++ src/ClienteDatabase.java
@@ -1,16 +1,30 @@
+// Essa classe tem a função de adicionar novos clientes,
+// assim como criar o HashMap.
+
 import java.util.HashMap;
 public class ClienteDatabase {
     private HashMap<String, Cliente> mapaCliente;
+    private Serializador serializador;
 
     public ClienteDatabase() {
-        mapaCliente = new HashMap<>();
+        serializador = new Serializador();
+        mapaCliente = serializador.carregarClienteEmArquivo();
+        if(mapaCliente == null) {
+            mapaCliente = new HashMap<>();
+        }
     }
 
     public void addCliente(Cliente cliente) {
         mapaCliente.put(cliente.getCpf(), cliente);
+        serializador.salvarClienteEmArquivo(mapaCliente);
     }
 
     public Cliente pesquisarClientePorCpf(String Cpf) {
         return mapaCliente.get(Cpf);
     }
+
+    public HashMap<String, Cliente> getMapaCliente() {
+        return mapaCliente;
+    }
+
 }
